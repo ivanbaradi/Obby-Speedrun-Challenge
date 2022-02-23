445c580870c1104c0f1d3f8f09d2d96498985221
Ivan Baradi <ivan4ubc@yahoo.com>
2022-02-23 13:58:49 -0800

    files edited

--- Scripts/Gameplay/PlayerStats/Load-Data_SERVER.lua
+++ Scripts/Gameplay/PlayerStats/Load-Data_SERVER.lua
@@ -1,35 +1,47 @@
+--ReplicatedStorage
+ReplicatedStorage = game.ReplicatedStorage
+
 --[[Loads player's unlocked stages
 
 	Param(s):
 	player stages folder => "Stages" Folder
-	read data stages => readData[3] or Unlocked Stages
+	read data stages => readData[3] (Unlocked Stages from DataStore)
 ]]
 function loadStages(player, playerStagesFolder, readDataStages)
+	
+	print('Loading '..player.Name.."'s unlocked stages")
 
 	for _, unlockedStage in pairs(readDataStages) do
 
 		--Loads stage to the player's "Stages" folder
 		local loadStage = Instance.new('StringValue', playerStagesFolder)
 		loadStage.Name = unlockedStage[1]
+
 		--Loads player's best time to the stage
 		local loadBestTime = Instance.new('IntValue', loadStage)
 		loadBestTime.Name = 'Best Time'
 		loadBestTime.Value = unlockedStage[2]
+		
 		--Loads Finished Stage state to the stage
 		local loadHasFinishedStage = Instance.new('BoolValue', loadStage)
 		loadHasFinishedStage.Name = 'Has Finished This Stage'
 		loadHasFinishedStage.Value = unlockedStage[3]
+		print('Loaded => Stage '..loadStage.Name..' with a time of '..loadBestTime.Value..' secs, Stage completed: '..tostring(loadHasFinishedStage.Value))
+		
 		--Unlocks stage and writes best time on the client's stage UI
-		game.ReplicatedStorage:FindFirstChild('Unlock Stage On Player Join'):FireClient(player, loadStage.Name, loadBestTime.Value, loadHasFinishedStage.Value)
+		ReplicatedStorage:FindFirstChild('Unlock Stage On Player Join'):FireClient(player, loadStage.Name, loadBestTime.Value, loadHasFinishedStage.Value)
 	end
 end
 
 --[[Loads player's data
 
-	Read Data contains the following:
-	- readData[1] => Highest Stage
-	- readData[2] => Current Stage
-	- readData[3] => Stages (Unlocked)
+	Param(s):
+	player => player object
+	readData => {
+		1 => Max Stage,
+		2 => Current Stage,
+		3 => List of UnlockedStages
+	}
 ]]
 script.Parent:FindFirstChild('LOAD DATA').Event:Connect(function(player, readData)
 
@@ -39,11 +51,11 @@ script.Parent:FindFirstChild('LOAD DATA').Event:Connect(function(player, readDat
 
 	--Loads player's highest stage
 	player.leaderstats['Max Stage'].Value = readData[1]
-	print("Highest Stage: "..player.leaderstats['Max Stage'].Value)
+	print("Loaded => Max Stage: "..player.leaderstats['Max Stage'].Value)
 
 	--Loads player's current stage
 	player['Current Stage'].Value = readData[2]
-	print("Current Stage: "..player['Current Stage'].Value)
+	print("Loaded => Current Stage: "..player['Current Stage'].Value)
 
 	--Loads player's stages
 	local Stages = Instance.new('Folder', player)
--- Scripts/Gameplay/PlayerStats/Save-Data_SERVER.lua
+++ Scripts/Gameplay/PlayerStats/Save-Data_SERVER.lua
@@ -5,13 +5,20 @@
 	write data => data to be saved
 ]]
 local function saveStages(playerStages, writeData)
+	
 	--Saves player's unlocked stages
-	local saveStages = {}
+	local unlockedStages = {}
 
 	for i, unlockedStage in pairs(playerStages:GetChildren()) do
 
-			--[[Includes the stage number, best time, and 'Has Finished This Stage' state; 
-				ex) stageData = {2, 60, true} ]]
+		--[[Table includes the following:
+		
+			stageData = {
+				1 => stage number (string),
+				2 => player's record of beating the obby,
+				3 => true if the player has finishd the stage
+			}
+		]]
 		local stageData = {} 
 
 		--Adds stage number, best time to stageData
@@ -20,11 +27,12 @@ local function saveStages(playerStages, writeData)
 		table.insert(stageData, 3, unlockedStage['Has Finished This Stage'].Value)
 		print('Saved => Stage '..unlockedStage.Name..' with a time of '..unlockedStage['Best Time'].Value..' secs, Stage completed: '..tostring(unlockedStage['Has Finished This Stage'].Value))
 
-		--Adds stage data to saveStages
-		table.insert(saveStages, i, stageData)
+		--Adds stage data to unlokcedStages
+		table.insert(unlockedStages, i, stageData)
 	end
-
-	table.insert(writeData, 3, saveStages)
+	
+	--Writes unlockedStages in writeData
+	table.insert(writeData, 3, unlockedStages)
 end
 
 --[[Saves the following data:
@@ -34,22 +42,34 @@ end
 ]]
 script.Parent:FindFirstChild('SAVE DATA').Event:Connect(function(player, BackpackStore)
 
-	--wait(.1)
 	print("Saving "..player.Name.."'s data")
+	
+	--[[Table that saves overall player's data. 
+	
+		writeData = {
+			1 => Max Stage
+			2 => Current Stage
+			3 => List of Unlocked Stages
+		}
+	]]
 	local writeData = {}
+	--Gets player's leaderstats
 	local leaderstats = player:WaitForChild('leaderstats')
 
 	--Saves player's highest stage
-	local highestStage = leaderstats:WaitForChild('Max Stage').Value
-	table.insert(writeData, 1, highestStage)
-	print('Highest Stage: '..highestStage..' saved!')
+	local maxStage = leaderstats:WaitForChild('Max Stage').Value
+	table.insert(writeData, 1, maxStage)
+	print('Saved => Max Stage: '..writeData[1])
+	
 	--Saves player's current Stage
 	local currentStage = player:WaitForChild('Current Stage').Value
 	table.insert(writeData, 2, currentStage)
-	print('Current Stage: '..currentStage..' saved!')
+	print('Saved => Current Stage: '..writeData[2])
+	
 	--Saves player's unlocked stages
 	saveStages(player:WaitForChild('Stages'), writeData)
-
+	
+	--Saves overall data
 	BackpackStore:SetAsync('User_'..player.UserId, writeData)
 	print(player.Name.."'s data saved")
 end)
