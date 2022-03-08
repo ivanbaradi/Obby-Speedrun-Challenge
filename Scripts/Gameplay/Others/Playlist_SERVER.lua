ab5ac1f8dd35edaa21eadb84084db6ea636c1d96
Ivan Baradi <ivan4ubc@yahoo.com>
2022-03-07 17:48:46 -0800

    files edited

--- Scripts/Gameplay/Others/Playlist_SERVER.lua
+++ Scripts/Gameplay/Others/Playlist_SERVER.lua
@@ -1,21 +1,21 @@
---Playlist Sound
+--Playlist 
 local Playlist = script.Parent
+--List of songs from the playlist
+local Songs = Playlist:FindFirstChild('Songs')
 
 --Playlist runs forever in the server
 while true do
-	for _, song in pairs(Playlist:GetChildren()) do
-		if song:IsA('IntValue') then
-			--Gets Sound ID
-			Playlist.SoundId = 'rbxassetid://'..song.Value
-			--Plays the song
-			Playlist:Play()
-			--Current time position of the song
-			local time_pos = 0
-			
-			repeat
-				wait(1)
-				time_pos += 1
-			until time_pos >= Playlist.TimeLength
-		end	
+	for _, song in pairs(Songs:GetChildren()) do
+		--Gets Sound ID
+		Playlist.SoundId = 'rbxassetid://'..song.Value
+		--Plays the song
+		Playlist:Play()
+		--Current time position of the song
+		local time_pos = 0
+		
+		repeat
+			wait(1)
+			time_pos += 1
+		until time_pos >= Playlist.TimeLength
 	end
 end
