82b1ccc508ec59b207e8695338992ee6a2853a13
ivanbaradi <ivanbaradi@gmail.com>
2025-11-10 11:20:03 -0800

    replaced deprecations

--- Scripts/Others/Disable-Player-Collision_SERVER.lua
+++ Scripts/Others/Disable-Player-Collision_SERVER.lua
@@ -1,29 +1,29 @@
 --ServerStorage
 ServerStorage = game.ServerStorage
+
 --Physics Service
 PhysicsService = game:GetService('PhysicsService')
---Sets the name of the collision group
-collisionGroupName = 'Players'
-PhysicsService:CreateCollisionGroup(collisionGroupName)
+--Sets the name of the collision group for players
+collisionGroupPlayers = 'Players'
+PhysicsService:RegisterCollisionGroup(collisionGroupPlayers)
 --Disables players from colliding each other
-PhysicsService:CollisionGroupSetCollidable(collisionGroupName, collisionGroupName, false)
+PhysicsService:CollisionGroupSetCollidable(collisionGroupPlayers, collisionGroupPlayers, false)
 
---[[Disables player from colliding with another player
 
-	Param(s):
-	char => player's character
-]]
-ServerStorage:FindFirstChild('Disable Player Collision').Event:Connect(function(char)
-	--Character's part is not collidable
-	local function disableCollision(part)
+ServerStorage:FindFirstChild('Disable Player Collision').Event:Connect(function(char: Model)
+	
+	--Makes part uncollidable
+	local function disableCollision(part: BasePart)
 		if part:IsA('BasePart') then
-			PhysicsService:SetPartCollisionGroup(part, collisionGroupName)
+			part.CollisionGroup = collisionGroupPlayers
 		end
 	end
-
+	
+	--Goes through every instance in the player's character
 	for _, part in pairs(char:GetChildren()) do
 		disableCollision(part)
 	end
-
+	
+	--Fires whenever an instance gets added to the character
 	char.ChildAdded:Connect(disableCollision)
 end)
